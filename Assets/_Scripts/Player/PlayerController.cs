81b8aff467a7699d1188b9f4a40296bb9be457f1
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-02-20 19:41:47 +0700

    Test Texture

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private MobileJoystick _playerJoystick;
     [SerializeField] private float _moveSpeed;
-    [SerializeField] private GameObject _Model;
+    [SerializeField] private GameObject _model;
 
     //private CharacterController controller;
     private Rigidbody rb;
@@ -25,20 +25,25 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
         // Lấy giá trị từ joystick (chỉ dùng X, Z vì game 3D)
         Vector2 input = _playerJoystick.GetMoveVector();
         Vector3 move = new Vector3(input.x, 0, input.y) * _moveSpeed * Time.deltaTime;
 
-        //// Di chuyển nhân vật
+        // Di chuyển nhân vật
         rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
 
         //xoay nhân vật theo hướng chuyển động
         if (move != Vector3.zero)
         {
-            _Model.transform.rotation = Quaternion.LookRotation(move * Time.deltaTime);
+            _model.transform.rotation = Quaternion.LookRotation(move * Time.deltaTime);
         }
 
-        // **Kiểm tra tốc độ để cập nhật Animation**
+        // Kiểm tra tốc độ để cập nhật Animation
         if (move.magnitude > 0.1f) // Nếu có di chuyển
         {
             ChangeAnimationState(AnimationState.Move);
