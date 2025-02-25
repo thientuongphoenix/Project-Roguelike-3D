4949ddd6f78b17e9b8ef47f9e588de6b740d9eb1
NgoTranBaoPhuc <ngotranbaophuc3.com>
2025-02-25 19:58:00 +0700

    Create Enemy

---
+++ Assets/_Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;  // Đối tượng Player
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
