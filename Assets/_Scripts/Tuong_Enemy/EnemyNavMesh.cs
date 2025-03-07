1f6b39cb5be5c42eace093481d60e60a4137abb6
thientuongphoenix <95729186+thientuongphoenix@users.noreply.github.com>
2025-03-07 08:56:55 +0700

    Enemy Climb Obstacle Complete
    Finally, I did it, beautiful !!!!!!!!!!!!!!

--- Assets/_Scripts/Tuong_Enemy/EnemyNavMesh.cs
+++ Assets/_Scripts/Tuong_Enemy/EnemyNavMesh.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyNavMesh : MonoBehaviour
@@ -9,13 +9,45 @@ public class EnemyNavMesh : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // Kiểm tra nếu Agent chưa được đặt trên NavMesh, thì tìm vị trí hợp lệ gần nhất
+        if (!agent.isOnNavMesh)
+        {
+            RepositionToNavMesh();
+        }
     }
 
     void Update()
     {
-        if (player != null)
+        CheckNavMeshPosition(); // Kiểm tra nếu Enemy bị rơi khỏi NavMesh
+
+        if (player != null && agent.isActiveAndEnabled && agent.isOnNavMesh)
         {
             agent.SetDestination(player.position);
+        }
+    }
+
+    void CheckNavMeshPosition()
+    {
+        if (!agent.isOnNavMesh)
+        {
+            Debug.LogWarning($"{gameObject.name} is off NavMesh! Trying to reposition...");
+            RepositionToNavMesh();
+        }
+    }
+
+    void RepositionToNavMesh()
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(transform.position, out hit, 5f, NavMesh.AllAreas))
+        {
+            transform.position = hit.position; // Dịch Enemy về vị trí hợp lệ
+            agent.Warp(hit.position); // Đưa AI trở lại NavMesh
+            Debug.Log($"{gameObject.name} repositioned to valid NavMesh area.");
+        }
+        else
+        {
+            Debug.LogError($"{gameObject.name} could not find a valid NavMesh position!");
         }
     }
 }
